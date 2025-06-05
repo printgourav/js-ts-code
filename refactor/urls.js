9fbfe9810e8c4b453725bf3a831c27bfc81e37b2
Gourav Tiwary <90273558+printgourav@users.noreply.github.com>
2025-06-06 01:07:03 +0530

    Update urls.js
    Used hasOwnProperty of Object has own or other object property and using includes for avoiding double "?" i.e query string and used "&" i.e seperator in case of multiple Parameter appending with Url.

--- refactor/urls.js
+++ refactor/urls.js
@@ -1,16 +1,15 @@
-// Refactor this code
-
-function getURL(dev = false, tasks = {}) {
-    let url = dev
-        ? `/tasks?status=ACTIVE&dev=true&size=20`
-        : '/tasks';
-
-    if (tasks.nextTasks) {
-        url += '?hasNext=true';
-    }
-
-    if (tasks.prevTasks) {
-        url = '/tasks?hasPrev=true';
-    }
-    return { url };
+function getURL(dev = false, task ={}){
+       let url = dev ? `tasks?status=ACTIVE&dev=true&size=20`: `/task`;
+       const params = [];
+       if(task.hasOwnProperty('nextTasks') && task.nextTasks){
+         params.push('hasNext=true');
+       }
+       if(task.hasOwnProperty('prevTask') && task.prevTasks){
+          params.push('hasPrev=true');
+       }
+       if(params.length > 0){
+       url += url.includes('?') ? '?' & '&';
+       url += params.join('&');
+       return {url};
+       }  
 }
